5d8dd8a11e5ac7a6013a9db37bc9b98d9fb45cb3
Vivek <vivekpahilwan@gmail.com>
2022-12-26 22:10:14 +0530

    added menu bar

--- AJPimp/menueg.java
+++ AJPimp/menueg.java
@@ -1,14 +1,33 @@
 
 import javax.swing.*;
 import java.awt.*;
-public class menueg extends Frame {
-    menueg(){
 
-       
+public class menueg extends Frame {
+    menueg() {
+        setVisible(true);
+        setSize(500, 500);
+        setLayout(new FlowLayout());
+
+        MenuBar mb = new MenuBar();
+        Menu m1 = new Menu("FILE");
+        Menu m2 = new Menu("EDIT");
+        Menu m3 = new Menu("VIEW");
+
+        MenuItem f1 = new MenuItem("NEW");
+        MenuItem f2 = new MenuItem("OPEN");
+
+        mb.add(m1);
+        mb.add(m2);
+        mb.add(m3);
+
+        m1.add(f1);
+        m1.add(f2);
+
+        setMenuBar(mb);
 
     }
-    public static void main(String args[])
-    {
-       new menueg();
+
+    public static void main(String args[]) {
+        new menueg();
     }
 }
