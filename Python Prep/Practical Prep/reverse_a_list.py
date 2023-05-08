ed35ecc1f5be03477ccbaea701af86cb18477714
vivekpahilwan <vivekpahilwan@gmail.com>
2023-05-08 23:44:09 +0530

    Reverse

---
+++ Python Prep/Practical Prep/reverse_a_list.py
@@ -0,0 +1,6 @@
+list1 = [1, 3, 4, 5, 6, 7, 8, 9]
+list1.reverse()
+print("By using the reverse() function:", list1)
+
+list2 = [1, 3, 4, 5, 6, 7, 8, 9]
+print("By using range function:", list2[::-1])
