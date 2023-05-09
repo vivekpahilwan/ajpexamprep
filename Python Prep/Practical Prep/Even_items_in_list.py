44b90a3c600cc7e832fa25551fee091612197ac2
vivekpahilwan <vivekpahilwan@gmail.com>
2023-05-09 22:54:52 +0530

    Even items in list updated

--- Python Prep/Practical Prep/Even_items_in_list.py
+++ Python Prep/Practical Prep/Even_items_in_list.py
@@ -1,3 +1,8 @@
 list = list(int(n) for n in input("Enter items in list: ").split())
-print(list)
+# Printing Even Index Items
 print(list[1::2])
+
+# Printing Even elements in List
+list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_list = [i for i in list2 if i % 2 == 0]
+print(even_list)
